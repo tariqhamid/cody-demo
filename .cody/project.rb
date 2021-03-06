60221b06bd6924f1a70b919871136c750ff89664
Your Name <you@example.com>
2021-03-06 19:35:34 +0000

    initial commit

---
+++ .cody/project.rb
@@ -0,0 +1,15 @@
+# For methods, refer to the properties of the CloudFormation CodeBuild::Project https://amzn.to/2UTeNlr
+# For convenience methods, refer to the source https://github.com/tongueroo/cody/blob/master/lib/cody/dsl/project.rb
+# Docs:
+#
+# * https://cody.run/docs/dsl/project/
+# * https://cody.run/docs/docs/dsl/helpers/
+#
+
+# name("example-project-name") # recommend leaving unset and codebuild will use a conventional name
+github_url("https://github.com/user/repo")
+linux_image("aws/codebuild/amazonlinux2-x86_64-standard:3.0")
+environment_variables(
+  JETS_ENV: "test",
+  # API_KEY: "ssm:/codebuild/demo/api_key" # Example of ssm parameter
+)
